ad5485ba20ea17984a4be0cdbe5ed7b980d67a3e
CarRamos <goodsleep415@gmail.com>
2022-03-02 15:22:44 -0800

    started the react state challenge

---
+++ state-carlos/src/App.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react'
+import Boxes from './components/Boxes'
+
+class App extends Component {
+  render() {
+    return (
+    <>
+    <Boxes />
+    </>
+    )
+  }
+}
+
+export default App
