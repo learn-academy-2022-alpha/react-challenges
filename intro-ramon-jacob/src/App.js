ed6a31df689d8e5457fb1b2aa2dcb30cd495c7e1
Valeriano-WebDev <Valeriano-WebDev@gmail.com>
2022-03-02 14:52:48 -0800

    Refactored. Exercise completed

--- intro-ramon-jacob/src/App.js
+++ intro-ramon-jacob/src/App.js
@@ -3,17 +3,17 @@ import Header from './components/Header'
 import Profile from "./components/Profile";
 import Favorites from "./components/Favorites";
 import Footer from "./components/Footer";
-import Test from "./components/Test";
+
 
 class App extends Component {
   render(){
     return(
         <>
-          < Header />
-          < Profile />
-          < Favorites />
-          < Footer />
-          < Test />
+          <Header/>
+          <Profile/>
+          <Favorites/>
+          <Footer/>
+
         </>
     )
   }
