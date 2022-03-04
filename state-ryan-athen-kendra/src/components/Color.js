87f9f84094c0a9fb6b813fdd172b9ff1ea408944
Athen Park <athenjpark@gmail.com>
2022-03-03 21:10:11 -0800

    Modifications to Color.js, made more progress

--- state-ryan-athen-kendra/src/components/Color.js
+++ state-ryan-athen-kendra/src/components/Color.js
@@ -3,22 +3,53 @@ class Color extends Component {
     constructor(props){
         super(props)
         this.state = {
-            colors: ["green", "blue", "yellow", "red", "purple", "orange"]
+            colors: ["green", "blue", "yellow", "red", "purple", "orange"],
+            // greenColor: "green",
+            // blueColor: "blue",
+            // redColor: "red",
             count: 0
         }
     }
     colorChanger = () => {
-      if(count > 5){
-        this.setState({this.state.colors[0]})
+      for(let i= 0; i<this.state.colors.length; i++){
+      if({count: this.state.count} > 6){
+        this.setState({colors: this.state.colors[0]})
       } else{
         this.setState({count: this.state.count + 1})
+        this.setState({colors: this.state.colors[this.state.count]})
       }
     }
+  }
+        // for(let i = 0; i<this.state.colors.length; i++){
+        //   if(this.state.colors[i] === "green"){
+        //     this.setState({colors:this.state.colors[0]})
+        //   } else if(this.state.colors[i] === "blue"){
+        //     this.setState({colors:this.state.colors[1]})
+        //   } else if(this.state.colors[i] === "yellow"){
+        //     this.setState({colors:this.state.colors[2]})
+        //   } else if(this.state.colors[i] === "red"){
+        //     this.setState({colors:this.state.colors[3]})
+        //   } else if(this.state.colors[i] === "purple"){
+        //     this.setState({colors: this.state.colors[4]})
+        //   } else if(this.state.colors[i]=== "orange"){
+        //     this.setState({colors:this.state.colrs[5]})
+        //   } else{
+        //     i = this.state.colors.length - this.state.colors.length
+        //   }
+        //   }
+        // }
+
+
+
+
+
+
 
     render() {
       return (
        <>
-        <div id="square"
+        <div id="square">
+        <p> Color: {this.state.colors} </p>
         <button onClick={this.colorChanger}>
             Click this Button
         </button>
@@ -27,5 +58,8 @@ class Color extends Component {
       )
     }
   }
+
+
+
 
   export default Color
