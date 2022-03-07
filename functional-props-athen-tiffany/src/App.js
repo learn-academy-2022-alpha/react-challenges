898990704d676b0a5b7a00505ee64ebd92e87b8a
Athen Park <athenjpark@gmail.com>
2022-03-07 15:56:59 -0800

    Added another list

--- functional-props-athen-tiffany/src/App.js
+++ functional-props-athen-tiffany/src/App.js
@@ -8,10 +8,19 @@ class App extends Component {
     this.state = {
       mcDonalds: ["chicken nuggets", "french fries", "big mac", "sweet tea", "sprite", "mcrib", "mcchicken", "coke"],
       inNOut: ["hamburgers", "animal fries", "milkshakes", "french fries", "double doubles", "fountain drink"],
-      cart: []
+      jackInTheBox: ["tacos", "curly fries", "munchie meal", "iced coffee", "jumbo jack", "crispy chicken strips", "spicy chicken sandwich"],
+      // name: [ item: "nuggets", price: 1.00
+      //         item: "soda", price: 1.50
+      //         item: "big mac", price: 4.50
+      //       ],
+      cart: [],
+      currentCost: 0
     }
   }
 
+// addCost = (item) => {
+//   this.setState({currentCost: this.state.name.price})
+// }
 
 addItem = (item) => {
   this.setState({cart: [...this.state.cart, item]})
@@ -22,9 +31,11 @@ render() {
   return(
     <>
     <h1>Order From McDonalds:</h1>
-    <List cartItem = {this.state.mcDonalds} addItem={this.addItem} />
+    <List cartItem = {this.state.mcDonalds} addItem={this.addItem}  />
     <h1>Order from In-N-Out:</h1>
     <List cartItem = {this.state.inNOut} addItem={this.addItem} />
+    <h1> Order from Jack in the Box </h1>
+    <List cartItem= {this.state.jackInTheBox} addItem={this.addItem} />
 
     <h3>Cart:</h3>
     <ul>
