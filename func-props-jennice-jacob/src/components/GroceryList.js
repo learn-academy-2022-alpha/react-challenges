6c189e44c0cf339cebd8ab52c1015beabcc1f380
Jennice <jennicelodi@gmail.com>
2022-03-07 17:22:18 -0800

    functional props exercise, has an item undefined error

---
+++ func-props-jennice-jacob/src/components/GroceryList.js
@@ -0,0 +1,18 @@
+import React, { Component } from 'react'
+
+class GroceryList extends Component {
+    render(){
+        return(
+            <>
+            {this.props.cartItem.map((item,index) => {
+            return(
+                <li key={index}>
+                <button onClick={() => {this.props.addItem(this.item)}}>{item}</button>
+                </li>
+            )})}    
+            </>
+        )
+        }
+    }
+
+export default GroceryList 
