13283a0fff2c402b244a6e2d1cef4b80a89e82db
keelanpattersoncaesar <keelanpattersoncaesar@gmail.com>
2022-03-07 16:18:13 -0800

    finished function props challenge

---
+++ func-props-nate-keelan/src/components/Header.js
@@ -0,0 +1,15 @@
+import React, { Component } from 'react'
+
+
+class Header extends Component{
+  render(){
+    return(
+      <>
+        <h1>Food Ordering App</h1>
+      </>
+      
+    )
+  }
+}
+
+export default Header
