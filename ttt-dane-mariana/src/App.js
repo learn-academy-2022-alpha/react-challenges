38b6f233edc435a3bccb46bda6b0cae1264417a7
Dane Roelofs <dane.l.roelofs@gmail.com>
2022-03-09 12:14:40 -0800

    still working on tic tac toe

--- ttt-dane-mariana/src/App.js
+++ ttt-dane-mariana/src/App.js
@@ -11,7 +11,9 @@ class App extends Component {
       squares: ["", "", "", "", "", "", "", "", ""],
       firstTurn: "",
       secondTurn: "",
-      isFirstPersonsTurn: true
+      isFirstPersonsTurn: true,
+      disabled: false,
+      winner: ""
     }
   }
 
@@ -25,9 +27,18 @@ class App extends Component {
     }
   }
 
+  componentDidUpdate() {
+    if (!this.state.disabled) {
+      const winner = this.checkForWinner()
+      if (winner) {
+        this.setState({ winner: winner })
+      }
+    }
+  }
+
   handleClick = (index) => {
     const { squares, firstTurn, secondTurn, isFirstPersonsTurn } = this.state
-    if (squares[index] !== "") return
+    if (this.state.disabled || squares[index] !== "") return
     else {
       const prevBoard = [...squares]
       if (isFirstPersonsTurn) {
@@ -37,24 +48,29 @@ class App extends Component {
         prevBoard[index] = secondTurn
       }
       this.setState({ squares: prevBoard, isFirstPersonsTurn: !isFirstPersonsTurn })
-    } 
-    this.checkForWinner()
+    }
   }
 
   checkForWinner = () => {
-    const winner = [[0,1,2],[3,4,5],[6,7,8],[0,4,8],[2,4,6],[0,3,6],[1,4,7],[2,5,8]] 
-  for (let i = 0; i < winner.length; i++){
-let element = winner[i] 
-let combo = element.map(value => this.state.squares[value])
-console.log(combo)
-  }
+    const winner = [[0, 1, 2], [3, 4, 5], [6, 7, 8], [0, 4, 8], [2, 4, 6], [0, 3, 6], [1, 4, 7], [2, 5, 8]]
+    for (let i = 0; i < winner.length; i++) {
+      const element = winner[i]
+      const combo = element.map(value => this.state.squares[value])
+      const didWin = combo.every(value => combo[0] !== "" && combo[0] === value)
+      if (didWin) {
+        this.setState({ disabled: true })
+        return combo[0]
+      }
+    }
   }
 
   render() {
     return (
       <>
         <h1>Tic Tac Toe</h1>
-        {this.state.firstTurn + " goes first"}
+        {
+          this.state.winner ? this.state.winner + " IS THE WINNER!!!" : ""
+        }
         <div className="gameboard">
           {this.state.squares.map((value, index) => {
             return (
