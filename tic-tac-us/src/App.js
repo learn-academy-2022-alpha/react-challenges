c83e1673d6eb059346e00ede64d4a92de8b76f62
CarRamos <goodsleep415@gmail.com>
2022-03-09 10:49:24 -0800

    day two of tic-tac-do

--- tic-tac-us/src/App.js
+++ tic-tac-us/src/App.js
@@ -20,7 +20,36 @@ handleMoves = (index) => {
     })
 }
 
+winnerPath = () => {
+  const {squares} = this.state
+  let lines = [
+    [squares[0], squares[1], squares[2]],
+    // [3, 4, 5],
+    // [6, 7, 8],
+    // [0, 3, 6],
+    // [1, 4, 7],
+    // [2, 5, 8],
+    // [0, 4, 8],
+    // [2, 4, 6],
+  ];
+  for (let index = 0; index < lines.length; index++) {
+    const [a, b, c] = lines[index];
+    if (squares[a] && squares[a] === squares[b] && squares[a] === squares[c]) {
+      return squares[a];
+    }
+  }
+  return null;
+}
+
   render(){
+    const winner = this.winnerPath(this.state.squares);
+    let status;
+    if (winner) {
+      status = 'Winner: ' + winner;
+    } else {
+      status = 'Next player: ' + (this.state.xIsNext ? 'X' : 'O');
+    }
+   
     return(
       <>
       <h1>Tic Tac Toe</h1>
--- tic-tac-us/src/components/Square.js
+++ tic-tac-us/src/components/Square.js
@@ -2,6 +2,7 @@ import React, { Component } from 'react'
 
 class Square extends Component{
 handleClick = () => {
+    
 this.props.handleMoves(this.props.index)
 }
 
