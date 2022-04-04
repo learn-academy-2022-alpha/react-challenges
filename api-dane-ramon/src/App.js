9f5b2b714890640dd7d20f0b753d8804ac35da43
Dane Roelofs <dane.l.roelofs@gmail.com>
2022-04-04 14:29:24 -0700

    completed challenges

--- api-dane-ramon/src/App.js
+++ api-dane-ramon/src/App.js
@@ -1,32 +1,45 @@
 import React, { Component } from 'react'
 import logo from './logo.svg';
 import './App.css';
+require('dotenv').config()
 
 class App extends Component {
   constructor(props) {
     super(props)
-    this.state= {
+    this.state = {
       location: null,
+      photos: null
     }
   }
-  componentDidMount(){
+  componentDidMount() {
     fetch("https://ipapi.co/json/")
       .then(response => response.json())
-      .then(payload => this.setState({location: payload}))
+      .then(payload => this.setState({ location: payload }))
       .catch(error => console.log(error))
+    fetch("https://api.nasa.gov/mars-photos/api/v1/rovers/curiosity/photos?sol=1000&api_key=DEMO_KEY")
+      .then(response => response.json())
+      .then(payload => this.setState({ photos: payload.photos }))
   }
-  render(){
-    
-    return(
-    <>
-    <h1></h1>
-    {this.state.location?<p> Your currency is {this.state.location.currency}
-    <p> You live in  {this.state.location.region}</p></p>:null}
-    
-    </>
+  render() {
+    return (
+      <>
+        {this.state.location ? <p> Your currency is {this.state.location.currency}
+          <p> You live in  {this.state.location.region}</p></p> : null}
+        <h1>Some Pictures of Dirt</h1>
+        {
+          this.state.photos ?
+            <>
+              {
+                this.state.photos.map((value, i) => (
+                  <img key={i} src={value.img_src} alt="" style={{ width: '300px' }} />
+                ))
+              }
+            </> : null
+        }
+
+      </>
     )
   }
-
 }
 
 export default App;
